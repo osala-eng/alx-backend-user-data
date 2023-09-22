1b3bb97044b78b161bc28c6dcf24cd4254b83d53
Jashon Osala <jashonosala@gmail.com>
2023-09-22 05:06:51 +0300

    chore: Update filtered logger.

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-""" Encrypting pswds with bcrypt """
+"""Encrypting pswds with bcrypt."""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ Takes in string arg, converts to unicode
+    """
+    Takes in string arg, converts to unicode
     Returns salted, hashed pswd as bytestring
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Checks if hashed and unhashed pswds are same
+    """
+    Checks if hashed and unhashed pswds are same
     Returns bool
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
